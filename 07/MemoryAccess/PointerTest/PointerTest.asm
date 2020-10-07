b8b54aabc0e42c314c49a7a75a2014ae712a1c19
Golan Cohen <golan13@gmail.com>
2020-10-07 10:49:57 +0300

    Test files

---
+++ 07/MemoryAccess/PointerTest/PointerTest.asm
@@ -0,0 +1,124 @@
+//push constant 3030
+@3030
+D=A
+@SP
+M=M+1
+A=M-1
+M=D
+//pop pointer 0
+@SP
+AM=M-1
+D=M
+@THIS
+M=D
+//push constant 3040
+@3040
+D=A
+@SP
+M=M+1
+A=M-1
+M=D
+//pop pointer 1
+@SP
+AM=M-1
+D=M
+@THAT
+M=D
+//push constant 32
+@32
+D=A
+@SP
+M=M+1
+A=M-1
+M=D
+//pop this 2
+@2
+D=A
+@THIS
+D=M+D
+@13
+M=D
+@SP
+AM=M-1
+D=M
+@13
+A=M
+M=D
+//push constant 46
+@46
+D=A
+@SP
+M=M+1
+A=M-1
+M=D
+//pop that 6
+@6
+D=A
+@THAT
+D=M+D
+@13
+M=D
+@SP
+AM=M-1
+D=M
+@13
+A=M
+M=D
+//push pointer 0
+@THIS
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+//push pointer 1
+@THAT
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+//add
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+D=M+D
+M=D
+//push this 2
+@2
+D=A
+@THIS
+A=M+D
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+//sub
+@SP
+M=M-1
+A=M
+D=-M
+A=A-1
+D=M+D
+M=D
+//push that 6
+@6
+D=A
+@THAT
+A=M+D
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+//add
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+D=M+D
+M=D
